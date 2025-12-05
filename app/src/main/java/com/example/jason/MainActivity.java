68392167df167124c2d78dc6ff5d4fffa99af9e3
Maciej876 <maciek.falek4@gmail.com>
2025-12-05 12:11:36 +0100

    polaczenie z internetem i odczyt pytan wsywietlenie tresci w toast

--- app/src/main/java/com/example/jason/MainActivity.java
+++ app/src/main/java/com/example/jason/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.jason;
 
 import android.os.Bundle;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,45 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://my-json-server.typicode.com/Maciej876/retrofit_pytania/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<List<pytanie>> call = jsonPlaceHolderApi.getPytania();
+        call.enqueue(
+                new Callback<List<pytanie>>() {
+                    @Override
+                    public void onResponse(Call<List<pytanie>> call, Response<List<pytanie>> response) {
+                        if(!response.isSuccessful()){
+                            Toast.makeText(MainActivity.this, response.code(), Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        List<pytanie> pytania = response.body();
+                        Toast.makeText(MainActivity.this, pytania.get(0).getTrescPytania(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<pytanie>> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
     }
 }
